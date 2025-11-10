d5321af9346f61c699c82ee85bb3652bafb494ef
Ken Vilar <kenvilar@gmail.com>
2025-11-10 17:46:22 +0800

    Add function to enqueue child theme styles with versioning based on filemtime

--- functions.php
+++ functions.php
@@ -1,1 +1,17 @@
 <?php
+
+if ( ! function_exists( 'zengistics_child_theme_enqueue_styles' ) ) {
+	function zengistics_child_theme_enqueue_styles() {
+		$theme_dir     = get_stylesheet_directory();
+		$ver_style_css = file_exists( $theme_dir . '/style.css' ) ? filemtime( $theme_dir . '/style.css' ) : null;
+
+		/*wp_enqueue_style(
+			'zengistics-childstyle',
+			get_stylesheet_uri(),
+			[],
+			$ver_style_css
+		);*/
+	}
+
+	add_action( 'wp_enqueue_scripts', 'zengistics_child_theme_enqueue_styles' );
+}

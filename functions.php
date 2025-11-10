72d93104ae0bf9ca40b588130fb833de5048a9cf
Ken Vilar <kenvilar@gmail.com>
2025-11-10 17:46:41 +0800

    Fix incomplete wp_enqueue_style() call in functions.php

--- functions.php
+++ functions.php
@@ -5,12 +5,12 @@ if ( ! function_exists( 'zengistics_child_theme_enqueue_styles' ) ) {
 		$theme_dir     = get_stylesheet_directory();
 		$ver_style_css = file_exists( $theme_dir . '/style.css' ) ? filemtime( $theme_dir . '/style.css' ) : null;
 
-		/*wp_enqueue_style(
+		wp_enqueue_style(
 			'zengistics-childstyle',
 			get_stylesheet_uri(),
 			[],
 			$ver_style_css
-		);*/
+		);
 	}
 
 	add_action( 'wp_enqueue_scripts', 'zengistics_child_theme_enqueue_styles' );
